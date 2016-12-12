04d844fb74053f3115acf6f6731496975e1c8ef6
Connor Shearwood <drake@lifein2d.co.uk>
2016-12-12 01:21:07 +0000

    Title screen!

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        public void LoadSceneByIndex(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
